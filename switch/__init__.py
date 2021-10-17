5a760f5e128f6e557e8e277b879c18f5a3942cea
jon-edward <arithmatlic@gmail.com>
2021-10-16 23:18:49 -0400

    Documentation

--- switch/__init__.py
+++ switch/__init__.py
@@ -2,23 +2,35 @@ from __future__ import annotations
 
 
 class _InvalidCase:
+    """Sentinel object for defining when a case is not met."""
     pass
 
 
 _INVALID_CASE = _InvalidCase()
 
 
+#  A case is 'default' when its predicate is always true.
+#  Declaring a variable named 'default' is simply to increase
+#  readability for the user.
 default = True
 
 
 class switch:
+    """
+    Is a switch-case implementation.
+
+    Note: If a default value is defined before other values, it will always return before them no matter if they are
+    actually accepted or not.
+    """
     __slots__ = []
 
     def __call__(self, f: switch):
+        """This is called when `switch` is used as a decorator."""
         return switch.eval(f)
 
     @staticmethod
     def case(predicate):
+        """A decorator that defines default behavior for case function definitions in a class."""
         is_correct_case = bool(predicate)
 
         def decorator(function):
@@ -31,6 +43,7 @@ class switch:
         return decorator
 
     def eval(self):
+        """Resolves the switch statement, and returns the accepted case's returning value."""
         functions = [
             getattr(self, x) for x in self.__dict__ if
             not x.startswith("__") and x != "value"
